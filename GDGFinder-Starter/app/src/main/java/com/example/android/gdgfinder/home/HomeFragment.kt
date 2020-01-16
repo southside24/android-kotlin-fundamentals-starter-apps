980d77189446609b8b71b6bb00980fe425d54a10
southside24 <opdort24@gmail.com>
2020-01-16 15:39:44 +0100

    styles and theme complete

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -22,7 +22,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -39,6 +41,15 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer<Boolean>{ navigate ->
+            if(navigate){
+                val navController = findNavController()
+                navController.navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+                viewModel.onNavigatedToSearch()
+            }
+        })
 
         return binding.root
     }
